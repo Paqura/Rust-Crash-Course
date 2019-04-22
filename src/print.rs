c300b8a2debc4870908284941686e458495d52cb
paqura <bloodking666@mail.ru>
2019-04-22 16:03:00 +0400

    Work with print

--- src/print.rs
+++ src/print.rs
@@ -5,6 +5,9 @@ pub fn run() {
 	// Basic formatting
 	println!("{} is {}", "Slava", "Avals");
 
+	// Basic math
+	println!("10 + 10 = {}", 10 + 10);
+
 	// Positional arguments
 	println!(
 		"{0} is {1}, and {0}",
@@ -21,4 +24,7 @@ pub fn run() {
 	// По факту переводит в определенную систему исчесления
 	// Выведет 1010 а 12
 	println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+	// Placeholder for Debug
+	println!("{:?}", (123, "Hello world", true));
 }
